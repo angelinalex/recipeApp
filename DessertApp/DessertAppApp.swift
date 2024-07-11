c5bd38435972fb27c879c722bffdec7602266404
angelinajoy <agremina@gmail.com>
2024-07-11 17:06:23 -0400

    Initial Commit

---
+++ DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Angelina on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
